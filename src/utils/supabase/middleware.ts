fa4eb117b14082a2189e0bbeb153d294d8d57690
hanxxiiz <labanderohannah@gmail.com>
2025-10-30 18:39:26 +0800

    auth deployment fix .2

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -37,17 +37,19 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/error')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
+  const publicPaths = ['/', '/auth', '/error']
+  const isPublicPath = publicPaths.some(path => 
+    request.nextUrl.pathname === path || 
+    request.nextUrl.pathname.startsWith(`${path}/`)
+  )
+
+  if (!user && !isPublicPath) {
+    // no user, redirect to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/signin'
     return NextResponse.redirect(url)
   }
+
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
