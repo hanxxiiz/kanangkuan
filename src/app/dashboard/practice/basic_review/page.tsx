cdf265e5b3bca930a20ee6c42bb0483799b399f5
blackxswann <puertoshaynerose8@gmail.com>
2025-11-04 08:33:54 +0800

    imports tweaked

---
+++ src/app/dashboard/practice/basic_review/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DiVim } from "react-icons/di";
+
+const BasicReview = () => {
+    return(
+    <div className="bg-white min-h-screen">
+
+
+    </div>
+
+    );
+}
+
+export default BasicReview
