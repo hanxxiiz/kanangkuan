4ae869dbb0c8dcaa876c9aa6c328ec782862125b
Kawrr <abellacarla335@gmail.com>
2025-11-16 23:09:48 +0800

    feature/notification
    -converted next.js to pwa

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/kanangkuan-logo-512x512.png" />
+      <meta name="theme-color" content="#fff" />
+    </>
+  );
+}
