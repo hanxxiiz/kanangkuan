fc939c994e94b70b77ec9b936a744a511b499dea
hanxxiiz <labanderohannah@gmail.com>
2025-12-01 20:26:31 +0800

    please work

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -3,7 +3,9 @@ import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
-    request,
+    request: {
+      headers: request.headers,
+    },
   })
 
   const supabase = createServerClient(
@@ -15,7 +17,6 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll()
         },
         setAll(cookiesToSet) {
-          supabaseResponse = NextResponse.next({ request })
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
           )
@@ -44,7 +45,11 @@ export async function updateSession(request: NextRequest) {
     // no user, redirect to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/auth/signin'
-    return NextResponse.redirect(url)
+    const redirectResponse = NextResponse.redirect(url)
+    supabaseResponse.cookies.getAll().forEach(({ name, value, ...options }) => {
+      redirectResponse.cookies.set(name, value, options)
+    })
+    return redirectResponse
   }
 
 
